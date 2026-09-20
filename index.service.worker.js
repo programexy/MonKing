e4ec2ef87667f8826bd484889a6966b16e7f2a8c
Enoch Yeo <enoch@atlas-MacAir.local>
2026-09-19 18:08:39 -0700

    OHMYGOSH

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788496463|345200492258';
+const CACHE_VERSION = '1789866503|76256719322';
 /** @type {string} */
 const CACHE_PREFIX = 'Monking-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
